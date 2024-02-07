6c855d4e6d27d622a73ef4054bfa371a6a289932
Igor Korolevich <ikorolevich4@gmail.com>
2024-02-07 21:28:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Module7_HomeWork
+{
+    abstract class Delivery
+    {
+        protected string address;
+        public string Adress { get { return address; } }
+    }
+
+
+    class HomeDelivery : Delivery {
+        public HomeDelivery() { }
+        public HomeDelivery(string address)
+        {
+            this.address = address;
+        }
+    }
+
+    class PickPointDelivery : Delivery { /* ... */ }
+
+    class ShopDelivery : Delivery { /* ... */ }
+
+    class Order<TDelivery>
+    {
+        protected TDelivery delivery;
+        protected int number;
+        protected string description;
+        public TDelivery Delivery { get { return delivery; } }
+        public Order(TDelivery delivery) {           
+            this.delivery = delivery;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeDelivery delivery = new HomeDelivery("MyAdress");
+            Order<HomeDelivery> order = new Order<HomeDelivery>(delivery);
+            string address = order.Delivery.Adress;
+            Order<ShopDelivery> order1 = new Order<ShopDelivery>(new ShopDelivery());
+            
+        }
+    }
+}
