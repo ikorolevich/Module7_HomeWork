c4d92257fe6660344a8c49adfad292eea996c251
Igor Korolevich <ikorolevich4@gmail.com>
2024-02-19 11:19:20 +0300

    1.1

--- Program.cs
+++ Program.cs
@@ -2,32 +2,56 @@
 {
     abstract class Delivery
     {
-        protected string address;
-        public string Adress { get { return address; } }
+        public string shippingAddress { get; protected set; }
+        public Delivery( string address)
+        {
+            shippingAddress = address;
+        }
     }
 
 
     class HomeDelivery : Delivery {
-        public HomeDelivery() { }
-        public HomeDelivery(string address)
+        public HomeDelivery( string address ):base (address)
         {
-            this.address = address;
+            
         }
     }
 
-    class PickPointDelivery : Delivery { /* ... */ }
+    class PickPointDelivery : Delivery { 
+        public PickPointDelivery( string address ):base (address)
+        {
 
-    class ShopDelivery : Delivery { /* ... */ }
-
-    class Order<TDelivery>
-    {
-        protected TDelivery delivery;
-        protected int number;
-        protected string description;
-        public TDelivery Delivery { get { return delivery; } }
-        public Order(TDelivery delivery) {           
-            this.delivery = delivery;
         }
+    }
+
+    class ShopDelivery : Delivery { 
+        public ShopDelivery( string address ):base (address)
+        {
+
+        }
+    }
+
+
+    class Order<TDelivery> where TDelivery : Delivery
+    {
+        public int Number { get; private set; }
+        public string Description;
+        public TDelivery delivery { get; protected set; }
+        public Order(TDelivery delivery)
+        {
+            this.delivery = delivery;
+            GenerateNumber();
+        }
+
+        
+        public void DisplayAddress()
+        {
+            Console.WriteLine(delivery.shippingAddress);
+        }
+        private void GenerateNumber()
+        {
+            Number = new Random().Next(1,1000);
+        }      
     }
 
 
@@ -36,9 +60,10 @@
         static void Main(string[] args)
         {
             HomeDelivery delivery = new HomeDelivery("MyAdress");
-            Order<HomeDelivery> order = new Order<HomeDelivery>(delivery);
-            string address = order.Delivery.Adress;
-            Order<ShopDelivery> order1 = new Order<ShopDelivery>(new ShopDelivery());
+            Order<Delivery> myOrder = new Order<Delivery>(delivery);
+            myOrder.DisplayAddress();
+            
+            Environment.Exit(0);
             
         }
     }
